31ff500577f48be4e41fdac81500c10802394083
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-03-08 21:34:25 -0500

    fix CLIvia_api

--- clivia_api.rb
+++ clivia_api.rb
@@ -5,11 +5,11 @@ class CliviaApi
   include HTTParty
   base_uri "https://opentdb.com/api_token.php?command=request"
 
-  def self.index
+  def self.questions
     response_token = get(base_uri)
     base_url_with_token = "https://opentdb.com/api.php?amount=10&token="
-    token = JSON.parse(response_token.body, symbolize_name: true)
+    token = JSON.parse(response_token.body, symbolize_names: true)
     response = get("#{base_url_with_token}#{token[:token]}")
-    JSON.parse(response.body, symbolize_name: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
