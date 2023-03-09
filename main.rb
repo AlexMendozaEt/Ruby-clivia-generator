306dd72cace8a809eadc2cae6b7fbc9489bbfd90
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-03-09 00:24:22 -0500

    fix ClIvia api

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 require_relative "clivia_generator"
 
-
 custom_filename = ARGV.shift
 
-trivia = CliviaGenerator.new(custom_filename.nil? ? "scores.json" : new_file)
+trivia = CliviaGenerator.new(custom_filename.nil? ? "scores.json" : custom_filename)
 trivia.start
